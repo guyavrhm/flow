6a7625c9009f20d416cbeb6d41802be36f81b0ef
Guy Avraham <guy.avraham03@gmail.com>
2026-07-05 20:37:36 +0300

    refactor(mouse): convert LockedMouse to pynput event-driven listener

--- src/hardware/mouse.py
+++ src/hardware/mouse.py
@@ -3,11 +3,13 @@ Constants for string to pynput Button conversion +
 mouse functions
 """
 
+import time
 import threading
 from pynput.mouse import Button, Controller as MouseController, Listener as MouseListener
 
 import src.info.computerinfo as ci
 from src.ui.qtthread import flowThread
+
 
 mbuttons = {
     'left': Button.left,
@@ -28,38 +30,29 @@ mbuttons['x2'] = mbuttons['button9'] = t1
 mbuttons['x1'] = mbuttons['button8'] = t2
 
 
-class LockedMouse(flowThread):
+class LockedMouse(MouseListener):
     """
-    A thread inherited class used to collect mouse movement while keeping
-    the mouse pointer in the center.
+    A class used to collect mouse movement while keeping
+    the mouse pointer in the center, using pynput's event-driven MouseListener.
     """
 
     def __init__(self, on_move):
-        super(LockedMouse, self).__init__()
-
-        self.mouse = MouseController()
+        self.mouse_controller = MouseController()
         self.metrics = ci.get_screeninfo()
         self.x_center = int(self.metrics[0] / 2)
         self.y_center = int(self.metrics[1] / 2)
-
         self._callback = on_move
-        self._stopping = False
 
-    def run(self):
-        """
-        Callbacks mouse location relative to center of screen.
-        """
-        start_x = self.x_center
-        start_y = self.y_center
-        self.mouse.position = (start_x, start_y)
+        # Reset cursor to center initially
+        self.mouse_controller.position = (self.x_center, self.y_center)
+        super().__init__(on_move=self._on_move)
 
-        while not self._stopping:
-            if self.mouse.position != (start_x, start_y):
-                x_movement = self.mouse.position[0] - start_x
-                y_movement = self.mouse.position[1] - start_y
-                if not self._stopping:
-                    self.mouse.position = (start_x, start_y)
-                    self._callback(x_movement, y_movement)
+    def _on_move(self, x, y):
+        x_movement = x - self.x_center
+        y_movement = y - self.y_center
+        if x_movement != 0 or y_movement != 0:
+            self.mouse_controller.position = (self.x_center, self.y_center)
+            self._callback(x_movement, y_movement)
 
-    def stop(self):
-        self._stopping = True
+    def wait(self):
+        self.join()
